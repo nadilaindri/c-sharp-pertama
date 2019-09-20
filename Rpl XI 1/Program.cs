9313e58e5215eca9635b3acee5f3554bd851829a
nadilaindri <55537409+nadilaindri@users.noreply.github.com>
2019-09-20 10:58:40 +0700

    tugas

---
+++ Rpl XI 1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rpl_XI_1
+{
+    class Program
+    {
+   static void Main(string[] args)
+        {
+        string warna;
+        Console.Write("Masukkan warna:");
+        warna = Console.ReadLine();
+        if (warna == "merah" || warna == "merah")
+        {
+            Console.Write("\nWarna yang diinput adalah warna merah");
+        }
+        else if (warna == "biru")
+        {
+            Console.Write("\nWarna yang diinput adalah warna biru");
+        }
+        else
+        {
+            Console.Write("\nWarna yang diinput bukan warna merah dan biru");
+        }
+        Console.Read();
+
+
+
+
+        }
+    }
+}
